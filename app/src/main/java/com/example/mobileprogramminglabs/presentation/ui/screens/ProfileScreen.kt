3a942e6efb4794897d226123cd5916f0cf03adac
ilmagusinac <ilma.gusinac@stu.ibu.edu.ba>
2026-03-15 09:32:01 +0100

    Added Habits Screen Implementation

--- app/src/main/java/com/example/mobileprogramminglabs/presentation/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/mobileprogramminglabs/presentation/ui/screens/ProfileScreen.kt
@@ -1,34 +1,23 @@
 package com.example.mobileprogramminglabs.presentation.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.mobileprogramminglabs.R
 import com.example.mobileprogramminglabs.presentation.theme.DeepTeal
-import com.example.mobileprogramminglabs.presentation.theme.Thistle
 import com.example.mobileprogramminglabs.presentation.ui.components.InfoSection
+import com.example.mobileprogramminglabs.presentation.ui.components.ProfileSection
 import com.example.mobileprogramminglabs.presentation.ui.components.Title
 import com.example.mobileprogramminglabs.presentation.ui.util.InfoRowData
 
@@ -65,49 +54,6 @@ fun ProfileScreen(modifier: Modifier = Modifier) {
             )
         )
     }
-}
-
-@Composable
-fun ProfileSection(
-    name: String,
-    levelNo: String,
-    levelDescription: String,
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier = modifier
-            .size(dimensionResource(R.dimen.avatar_large))
-            .clip(CircleShape)
-            .background(Thistle),
-        contentAlignment = Alignment.Center
-    ) {
-        Icon(
-            imageVector = Icons.Default.Person,
-            contentDescription = stringResource(R.string.profile_avatar),
-            modifier = Modifier.size(40.dp)
-        )
-    }
-    Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
-    Text(
-        text = "$name Player",
-        style = MaterialTheme.typography.titleLarge,
-        color = DeepTeal,
-        fontWeight = FontWeight.Bold
-    )
-    Text(
-        text = "Level $levelNo Adventurer",
-        style = MaterialTheme.typography.bodyLarge,
-        color = DeepTeal,
-        fontWeight = FontWeight.Bold
-    )
-    Text(
-        text = levelDescription,
-        style = MaterialTheme.typography.bodyMedium,
-        color = DeepTeal,
-        fontStyle = FontStyle.Italic
-    )
-    Spacer(modifier = Modifier.height(24.dp))
-    
 }
 
 @Preview(showBackground = true, showSystemUi = true)
